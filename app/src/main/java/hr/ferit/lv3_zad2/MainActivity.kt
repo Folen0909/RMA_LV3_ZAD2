6b919a16a56c5e20474e9a24f4fc37591325ab12
Folen0909 <davidturkalj@gmail.com>
2021-04-21 20:40:45 +0200

    Initial commit

---
+++ app/src/main/java/hr/ferit/lv3_zad2/MainActivity.kt
@@ -0,0 +1,11 @@
+package hr.ferit.lv3_zad2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
